c7e3df6ed70e4f9afeb9a7e9b4f251adb68ce2d5
Alp Eren <erenalpturk@gmail.com>
2023-10-19 12:04:43 +0300

    exercise 1 completet

---
+++ NODE.JS/app.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+console.log("curves", crypto.getCurves()); // Kullanılabilecek eğrileri listeleyin
+console.log("hashes", crypto.getHashes()); // Kullanılabilecek hash fonksiyonlarını listeleyin
+console.log("ciphers",crypto.getCiphers());
+
+const randomId = crypto.checkPrime(2);
+console.log(randomId);
