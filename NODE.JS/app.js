d9aaec3c06c250d36838aa5e814c2626a358c349
Alp Eren <erenalpturk@gmail.com>
2023-11-11 07:08:33 +0300

    typescript exercise 1

--- NODE.JS/app.js
+++ NODE.JS/app.js
@@ -1,41 +1,17 @@
-// exercise 1
+var figlet = require("figlet");
+const express = require('express');
+const app = express();
+const port = 3000;
 
-//const crypto = require('crypto');
-
-// console.log("curves", crypto.getCurves()); // Kullanılabilecek eğrileri listeleyin
-// console.log("hashes", crypto.getHashes()); // Kullanılabilecek hash fonksiyonlarını listeleyin
-// console.log("ciphers",crypto.getCiphers());
-
-// const randomId = crypto.checkPrime(2);
-// console.log(randomId);
-
-
-
-//exercise 2
-// const hi = require('./module.js');
-
-// hi()
-
-
-// //exercise 3
-// function hi() {
-//     console.log("hello ecma.");
-// }
-
-// export default hi;
-
-
-
-// //exercise 4
-
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<html><body><h1>hello html</h1></body></html>');
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
 
-const port = 3000;
-server.listen(port, () => {
-    console.log(`server running ${port} port`);
+figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
 });
